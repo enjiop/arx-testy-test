994f4ceed97327c3cd46626eb7c35dbe6fe1441f
Yepanchyn Maksym <yepanchyn.m@brights.cloud>
2021-09-28 12:21:40 +0300

    remove generate.fallback at nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,9 +14,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   ssr: false,
-  generate: {
-    fallback: true
-  },
+  // generate: {
+  //   fallback: true
+  // },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head,
